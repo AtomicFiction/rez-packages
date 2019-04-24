2cac41847e454e2e2cb7991e8a262ff9babc19b6
Jonathan Llewellyn <jllewellyn@atomicfiction.com>
2019-04-24 13:06:16 -0400

    ptex: set gcc to 4.8.5, PTEX_LOCATION as base env var, removed LD_LIBRARY_PATH

--- ptex/2.1.28/package.py
+++ ptex/2.1.28/package.py
@@ -4,7 +4,7 @@ name = "ptex"
 version = "2.1.28"
 
 build_requires = [
-    'gcc-4.8.2+'
+    'gcc-4.8.5'
 ]
 
 requires = [
@@ -18,10 +18,5 @@ uuid = "ptex"
 
 def commands():
     env.PATH.append("{root}/bin")
-
-    if building:
-        env.PTEX_INCLUDE_DIR = '{root}/include'
-        env.PTEX_LOCATION = '{root}'
-        env.LD_LIBRARY_PATH.append('{root}/lib')
-
+    env.PTEX_LOCATION = '{root}'
 
