2af3532f1e440746fe66351f90d3b7c201528224
Jonathan Llewellyn <jllewellyn@atomicfiction.com>
2019-05-02 15:24:13 -0400

    opencolorio: set package.py to build macOS variant

--- opencolorio/1.0.9/package.py
+++ opencolorio/1.0.9/package.py
@@ -11,16 +11,19 @@ description = \
     OpenColorIO Color Management Framework
     """
 
-build_requires = [
-    "gcc-4.8.5",
+private_build_requires = [
+    #"gcc-4.8.5",
+    'clang-9.1.0'
 ]
 
 requires = [
-    "python-2.7"
+    "python-2.7",
+    'boost-1.55.0'
 ]
 
 variants = [
-    ["platform-linux", "arch-x86_64", "os-CentOS-7", "python-2.7"]
+    #["platform-linux", "arch-x86_64", "os-CentOS-7", "python-2.7"],
+    ['platform-osx', 'arch-x86_64', 'os-osx-10.13', 'python-2.7']
 ]
 
 tools = [
