0308b81bfecdd60cd6c36b2f970db1c513643702
Jonathan Llewellyn <jllewellyn@mtl-mini-002.atomicfiction.net>
2019-05-02 13:45:47 -0400

    boost: set package to build macOS variant

--- boost/1.55.0/package.py
+++ boost/1.55.0/package.py
@@ -11,13 +11,17 @@ description = \
     Peer-reviewed portable C++ source libraries.
     """
 
-build_requires = [
-    "gcc-4.8.5",
+private_build_requires = [
+    #'~gcc-4.8.5',
+    'clang-9.1.0',
     'python-2.7'
 ]
 
+
+
 variants = [
-    ["platform-linux", "arch-x86_64", "os-CentOS-7", "python-2.7"]
+    #["platform-linux", "arch-x86_64", "os-CentOS-7", "python-2.7"],
+    ['platform-osx', 'arch-x86_64', 'os-osx-10.13', 'python-2.7']
 ]
 
 uuid = "boost"
