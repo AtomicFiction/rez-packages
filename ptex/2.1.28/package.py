81008a2d50d9d141218d4243d2935766b58852cc
Jonathan Llewellyn <jllewellyn@atomicfiction.com>
2019-05-02 16:06:44 -0400

    ptex: set package.py to build macOS variant

--- ptex/2.1.28/package.py
+++ ptex/2.1.28/package.py
@@ -3,15 +3,17 @@ name = "ptex"
 
 version = "2.1.28"
 
-build_requires = [
-    'gcc-4.8.5'
+private_build_requires = [
+    #'gcc-4.8.5',
+    'clang-9.1.0'
 ]
 
 requires = [
 ]
 
 variants = [
-    ["platform-linux", "arch-x86_64", "os-CentOS-7"]
+    #["platform-linux", "arch-x86_64", "os-CentOS-7"],
+    ['platform-osx', 'arch-x86_64', 'os-osx-10.13']
 ]
 
 uuid = "ptex"
