50b92a036f4b0959e9a974a56fb1f801b3c53265
Jonathan Llewellyn <jllewellyn@atomicfiction.com>
2019-05-02 14:58:23 -0400

    ilmbase: set package.py to build macOS variant

--- ilmbase/2.2.0/package.py
+++ ilmbase/2.2.0/package.py
@@ -12,11 +12,13 @@ description = \
     """
 
 build_requires = [
-    "gcc-4.8.5"
+    #"gcc-4.8.5",
+    'clang-9.1.0'
 ]
 
 variants = [
-    ["platform-linux", "arch-x86_64", "os-CentOS-7"]
+    #["platform-linux", "arch-x86_64", "os-CentOS-7"],
+    ['platform-osx', 'arch-x86_64', 'os-osx-10.13']
 ]
 
 uuid = "ilmbase"
